5c410c99db3f3d8d289002abc4ac26cdc1bd00bf
ICELINK <icelink02@gmail.com>
2025-01-24 17:36:57 +0500

    initial project

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { ReactNode } from "react";
+import { StoreProvider } from "./StoreProvider";
+import { Nav } from "./components/Nav";
+
+import "./styles/globals.css";
+import styles from "./styles/layout.module.css";
+
+interface Props {
+  readonly children: ReactNode;
+}
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <StoreProvider>
+      <html lang="en">
+        <body>
+          <section className={styles.container}>
+            <header className={styles.header}>
+            <Nav />
+            </header>
+
+            <main className={styles.main}>{children}</main>
+
+            <footer className={styles.footer}>
+              
+            </footer>
+          </section>
+        </body>
+      </html>
+    </StoreProvider>
+  );
+}
